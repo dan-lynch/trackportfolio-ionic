91574bbdce13938fcf5e3d184a61f6d175fefa01
Daniel Lynch <daniel.lynch@tabcorp.com.au>
2020-08-10 23:29:30 +1000

    ios fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,35 @@
 import React, { useEffect } from 'react'
 import { withApollo } from '../components/withApollo'
 import { initApolloClient } from '../services/apolloService'
-import { Container, Typography, Box, Paper } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import Layout from '../components/Layout/LoggedOutLayout'
 import Modal from '../components/Modal'
 import LoginForm from '../components/Forms/Login'
 import SignUpForm from '../components/Forms/SignUp'
 import ForgotPassForm from '../components/Forms/ForgotPass'
 import { ModalOptions } from '../helpers/types'
-import { Alert } from '@material-ui/lab'
 import { authService } from '../services/authService'
+import { IonPage, IonContent, IonButton, IonIcon, IonLabel } from '@ionic/react'
+import { logInOutline, logInSharp, personAddOutline, personAddSharp } from 'ionicons/icons'
+import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles(() => ({
-  message: {
-    margin: '1rem 0',
-  },
   paper: {
-    padding: '1rem',
-    flex: '1 0 auto',
-    marginBottom: '1.5rem',
+    height: '20%',
+    width: '100%',
+    marginTop: '3rem',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  link: {
-    textDecoration: 'underline',
-    cursor: 'pointer',
-  },
+  logo: {
+		margin: '2rem 1rem 0 1rem',
+    maxWidth: '50%',
+    alignSelf: 'center',
+    textAlign: 'center'
+	},
 }))
 
 function Home() {
+
   const classes = useStyles()
-  
   const [currentModal, setCurrentModal] = React.useState<ModalOptions>(ModalOptions.None)
   const openLoginModal = () => setCurrentModal(ModalOptions.Login)
   const openSignupModal = () => setCurrentModal(ModalOptions.SignUp)
@@ -47,61 +47,56 @@ function Home() {
   }, [])
 
   return (
-    <Layout title='HOME' openLogin={openLoginModal} openJoin={openSignupModal}>
-      <Container maxWidth='md'>
-        <Box my={2}>
-          <Typography variant='h4' component='h1' gutterBottom>
-            Easily track your portfolio
-          </Typography>
-          <Paper className={classes.paper}>
-            <Typography variant='body1'>
-              <span className={classes.link} onClick={openSignupModal}>
-                Sign up
-              </span>{' '}
-              for your <strong>free</strong> account today and begin tracking your financial portfolio!
-              <br />
-              <br /> We already support <strong>8,000+</strong> US stocks and ETF's, along with 100+ Crypto coins and
-              all NZX (NZSX) companies.
-            </Typography>
-          </Paper>
-          <Alert variant='outlined' severity='info' className={classes.message}>
-            We're still building our platform, so expect lots of exciting changes soon!
-            <br />
-            <br />
-            <strong>Upcoming features:</strong>
-            <br />
-            - Support different currencies
-            <br />
-            - Revamped dashboard and more stocks!
-            <br />- Mobile app
-          </Alert>
-        </Box>
-        <Modal
-          open={currentModal === ModalOptions.Login}
-          onClose={closeModal}
-          label='Log in form'
-          title='Log in'
-          titleId='log-in'>
-          <LoginForm openSignupForm={openSignupModal} openForgotPassForm={openForgotPassModal} />
-        </Modal>
-        <Modal
-          open={currentModal === ModalOptions.SignUp}
-          onClose={closeModal}
-          label='Create your account form'
-          title='Create your account'
-          titleId='create-your-account'>
-          <SignUpForm openLoginForm={openLoginModal} />
-        </Modal>
-        <Modal
-          open={currentModal === ModalOptions.ForgotPass}
-          onClose={closeModal}
-          label='Forgot password form'
-          title='Forgot password'
-          titleId='forgot-password'>
-          <ForgotPassForm />
-        </Modal>
-      </Container>
-    </Layout>
+    <IonPage>
+      <IonContent>
+      <img src='/assets/icon/logo.svg' alt='trackportfol.io logo' className={classes.logo} />
+        <div className={classes.paper}>
+			<IonButton
+				onClick={openLoginModal}
+        expand='block'
+        color='secondary'
+				fill='outline'>
+				<IonIcon slot="start" ios={logInOutline} md={logInSharp} />
+				<IonLabel>LOGIN</IonLabel>
+			</IonButton>
+      <br />
+      </div>
+      <div className={classes.paper}>
+      <IonButton
+				onClick={openSignupModal}
+        expand='block'
+        color='secondary'
+				fill='outline'>
+				<IonIcon slot="start" ios={personAddOutline} md={personAddSharp} />
+				<IonLabel>REGISTER</IonLabel>
+			</IonButton>
+      </div>
+      </IonContent>
+      <Modal
+        open={currentModal === ModalOptions.Login}
+        onClose={closeModal}
+        label='Log in form'
+        title='Log in'
+        titleId='log-in'>
+        <LoginForm openSignupForm={openSignupModal} openForgotPassForm={openForgotPassModal} />
+      </Modal>
+      <Modal
+        open={currentModal === ModalOptions.SignUp}
+        onClose={closeModal}
+        label='Create your account form'
+        title='Create your account'
+        titleId='create-your-account'>
+        <SignUpForm openLoginForm={openLoginModal} />
+      </Modal>
+      <Modal
+        open={currentModal === ModalOptions.ForgotPass}
+        onClose={closeModal}
+        label='Forgot password form'
+        title='Forgot password'
+        titleId='forgot-password'>
+        <ForgotPassForm />
+      </Modal>
+    </IonPage>
   )
 }
 
